dd10473ab96d364f130aa0059d8d56efd4cec477
feno70 <fenoheriniainat@gmail.com>
2018-02-23 15:21:01 +0100

    ***************Ajout font Roboto-Regular*****************

--- resources/views/front/test/page/style-element.blade.php
+++ resources/views/front/test/page/style-element.blade.php
@@ -1,9 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	
-</body>
-</html>
+@extends('front.layout.master')
+
+@section('content')
+	<h1>Bonjour tous le monde</h1>
+	<span class="icon icon-heart"></span>
+	<button type="button" class="btn btn-default">SHOPPER</button>
+	<div class="title"><h2>INSTANGRAM FEED</h2></div>
+	<div class="title title-active"><h2>INSTANGRAM FEED</h2></div>
+	<div class="text-center small-title""><span>SHILTON</span></div>
+@endsection
