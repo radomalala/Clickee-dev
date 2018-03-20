6389b5c0805a0c98c76ab388e59170e8b4f99f03
Lydia Andrianjatov <lydiaandrianjatovo@gmail.com>
2018-03-20 09:31:05 +0300

    PRODUCT 01 wishlist icon

--- resources/views/front/product/index.blade.php
+++ resources/views/front/product/index.blade.php
@@ -209,7 +209,7 @@
 
                             <div class="row add-cart mb-40" id="add-cart">
                                 <!-- <p> {!! trans('product.product_available_with_selected_area') !!}</p> -->
-                                <button type="submit" class="btn btn-clickee-info col-md-9 col-xs-9 col-sm-9 col-sm-9" id="add-to-cart">{!! trans("product.add_to_cart")!!}</button>
+                                <button type="submit" class="btn btn-clickee-info col-md-8 col-xs-8 col-sm-8 col-sm-8" id="add-to-cart">{!! trans("product.add_to_cart")!!}</button>
 
                                 <?php 
                                         $wishlist_del = (in_array($product->product_id,all_product_id_wishlist())) ? 'coeur_gm' : '';
